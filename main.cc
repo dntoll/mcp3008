9e399f8c6a0efdb5c184ebb88a8d279303f8fd42
dntoll <daniel@familjentoll.se>
2014-11-30 13:27:00 +0100

    read all channels

--- main.cc
+++ main.cc
@@ -8,10 +8,14 @@ int main()
 {
 	mcp3008 inst;
 	
+	for (int i = 0;i < 1000; i++) {
+		for (int c = 0; c < 8; c++) {
+			int value = inst.readValue(c);
+			cout << " [" << value << "]";	
+		}
+		cout << "\n";	
 	
-	int value = inst.readValue(0);
-	
-	cout << "Read some value \n[" << value << "]";	
+	}
 	
 	return 0;
 }
